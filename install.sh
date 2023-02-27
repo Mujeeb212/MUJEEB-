57f3585e1ed391d302c6a557a249e040769ca222
Mujeeb212 <126510722+Mujeeb212@users.noreply.github.com>
2023-02-28 00:30:50 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash 
+ #code_recorded_by_tech_cochi 
+ clear 
+ sleep 1 
+ echo "" 
+ echo "" 
+ echo "  
+ ████████╗███████╗░█████╗░██╗░░██╗ 
+ ╚══██╔══╝██╔════╝██╔══██╗██║░░██║ 
+ ░░░██║░░░█████╗░░██║░░╚═╝███████║ 
+ ░░░██║░░░██╔══╝░░██║░░██╗██╔══██║ 
+ ░░░██║░░░███████╗╚█████╔╝██║░░██║ 
+ ░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝ " | lolcat 
+  
+  
+  
+  
+ echo "" 
+ sleep 1 
+ clear 
+  
+ pkg install php -y 
+ pkg install openssh -y 
+ pkg install unzip -y 
+ pkg install curl -y 
+ sleep 1 
+ clear 
+  
+ echo "Loading==> " | lolcat 
+ sleep 1 
+ clear 
+ echo "Loading====> " | lolcat 
+ sleep 1 
+ clear 
+ echo "Loading=======> " | lolcat 
+ sleep 1 
+ clear 
+ echo " done " | lolcat 
+ sleep 1 
+ clear 
+ unzip techcochi.zip 
+ clear 
+ rm -rf techcochi.zip 
+ clear 
+ bash about.sh 
+ sleep 4 
+ echo "" 
+ echo "" 
+ echo " SUBSCRIBE MY CHANNEL 🔔 " | lolcat 
+ xdg-open https://www.youtube.com/channel/HYDRAGAMING4U 
+ echo "" 
+ sleep 10 
+ echo " create account on cashkaro.com " 
+ echo " set your browser " 
+ termux-open-url https://cashkaro.com?r=8026255&fname=Rixon 
+ echo ""
